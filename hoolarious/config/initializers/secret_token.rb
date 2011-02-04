c2760f485a8786fcfdc3969e3336bac997a83358
unknown <joakim@.(none)>
2011-02-05 00:27:02 +0100

    brukersystem

---
+++ hoolarious/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoolarious::Application.config.secret_token = '89b5347014d3a9f2c7429fb731164222f4d8fc2a616d8804d9af9911b7c55e9ff7e8cf532c741b6bafe2916c57e2db7bdae4016816b15dceea0810f9f8edd6dc'
